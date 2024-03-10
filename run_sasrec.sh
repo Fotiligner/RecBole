2c748e90acf0e3eee29a2be1a28ba0d9fdf9eeaf
Fotiligner <3181945719@qq.com>
2024-03-10 13:14:46 +0000

    sasrec-plus

---
+++ run_sasrec.sh
@@ -0,0 +1,4 @@
+python run_recbole.py \
+    --model SASRec \
+    --train_neg_sample_args None \
+    --loss_type CE
---
+++ run_sasrec_plus.sh
@@ -0,0 +1,5 @@
+python run_recbole.py \
+    --model SASRec \
+    --train_neg_sample_args None \
+    --loss_type CE \
+    --kd_weight 0.5
